133741c2a335c835cfc8b4bd08fa5958ab37d60c
cdhardwick57@gmail.com <cdhardwick57@gmail.com>
2022-12-19 21:38:12 -0600

    updating files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,13 @@ SuspensionCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFac
 total_summary <- SuspensionCoil %>% summarise(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
 
 lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI), Variance = var(PSI), SD = sd(PSI) , .groups = 'keep')
+
+#################
+
+t.test(SuspensionCoil$PSI,mu=mean(SuspensionCoil$PSI))
+
+t.test(subset(SuspensionCoil$PSI,SuspensionCoil$Manufacturing_Lot == "Lot1"),mu=mean(SuspensionCoil$PSI))
+
+t.test(subset(SuspensionCoil$PSI,SuspensionCoil$Manufacturing_Lot == "Lot2"),mu=mean(SuspensionCoil$PSI))
+
+t.test(subset(SuspensionCoil$PSI,SuspensionCoil$Manufacturing_Lot == "Lot3"),mu=mean(SuspensionCoil$PSI))
